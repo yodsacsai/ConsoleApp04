1c3577936022a834164eb16230347e2c137ff7b1
yodsacsai <s6252410025@sau.ac.th>
2022-03-17 16:27:04 +0700

    V.1

---
+++ ConsoleApp04/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = { 10, 20, 30 };
+            try
+            {
+                Console.WriteLine(x[99]);
+            }
+            catch
+            {
+                Console.WriteLine("please contact admin");
+            }
+            finally
+            {
+                Console.WriteLine("wow wow wow");
+            }
+        }
+    }
+}
